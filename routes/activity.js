cece38fe08e70fef23b796338892806b302b43f3
crishna369 <crishna369@gmail.com>
2022-04-25 12:59:47 +0530

    added logger for debugging

--- routes/activity.js
+++ routes/activity.js
@@ -100,7 +100,7 @@ try {
         
         for(let i=0; i<uiConfigData.length;i++){
           // console.log("Requested data input id is ",uiConfigData[i].id);
-          // console.log("Requested data input is ",requestData.inArguments[0][uiConfigData[i].id]);
+          console.log("new line is "+uiConfigData[i].name+": "+requestData.inArguments[0][uiConfigData[i].id]+"\r\n");
 
           newContent += ""+uiConfigData[i].name+": "+requestData.inArguments[0][uiConfigData[i].id]+"\r\n";
         }
