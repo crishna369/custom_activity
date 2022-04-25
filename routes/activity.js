8201b2265b51b7d7785674c2bc288637a5af54d5
crishna369 <crishna369@gmail.com>
2022-04-25 17:52:04 +0530

    added log for debugging

--- routes/activity.js
+++ routes/activity.js
@@ -14,7 +14,7 @@ exports.execute = async (req, res) => {
   // decode data
    const requestData = JWT(req.body);
 
-  // console.log(data);
+   console.log("request data ",data);
 
   // try {
   //   const id = Uuidv1();
